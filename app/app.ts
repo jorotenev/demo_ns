b9e591d9d49684a1770ebc1c4f47f86060a7ab1d
Georgi Tenev <georgetenevbg@gmail.com>
2018-04-07 18:46:46 +0300

    refactor the navigation to the examples

--- app/app.ts
+++ app/app.ts
@@ -4,10 +4,16 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import "./bundle-config";
 import * as application from 'application';
 
-application.start({ moduleName: 'main-page' });
+const examples = {
+    main_page: 'main-page',
+    ui: 'examples/ui_showcase',
+    layouts: 'examples/example_layouts/example_layouts',
+    listview: 'examples/example_listview/example_listview',
+    observable: 'examples/example_observable'
+};
+application.start({moduleName: examples.layouts});
 
 /*
 Do not place any code after the application has been started as it will not
